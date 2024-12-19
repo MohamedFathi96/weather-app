a44b01db6a2373ac1192d0c73d484d54d7acd3b2
Mohamed Fathi <mohamedfathi9696@gmail.com>
2024-12-19 10:01:28 +0200

    🐛 FIX: replace api key

--- src/api/weatherClient.ts
+++ src/api/weatherClient.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const weatherApiClient = axios.create({
   baseURL: "https://api.weatherapi.com/v1",
   params: {
-    key: import.meta.env.VITE_WEATHER_API,
+    key: "60c0c4904e5c441fbb2224126241812",
   },
 });
 
