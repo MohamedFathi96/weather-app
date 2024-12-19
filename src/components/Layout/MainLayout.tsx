6492379040fd21b1379974ce1553f08acd848805
Mohamed Fathi <mohamedfathi9696@gmail.com>
2024-12-19 10:24:18 +0200

    🐛 FIX: Mainlayt width

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -5,7 +5,7 @@ const MainLayout = () => {
   return (
     <div className="bg-weather-image flex h-screen flex-col items-center justify-center bg-cover">
       <Navbar />
-      <div className="flex h-[70%] max-h-[450px] w-[60%] max-w-[550px] flex-col rounded-lg bg-gradient-to-r from-gray-800 via-blue-700 to-gray-900 p-6">
+      <div className="flex h-[70%] max-h-[450px] w-[90%] max-w-[550px] flex-col rounded-lg bg-gradient-to-r from-gray-800 via-blue-700 to-gray-900 p-6 md:w-[60%]">
         <Outlet />
       </div>
     </div>
